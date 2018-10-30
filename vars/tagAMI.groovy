2e9a788eb4f5c8c10a30052b7d4c5408dd39246b
rererecursive <patternseverywhere@yandex.com>
2018-10-30 22:49:07 +1100

    tagAMI: add cross-account AMI tagging.

--- vars/tagAMI.groovy
+++ vars/tagAMI.groovy
@@ -4,7 +4,13 @@
  Tags an AMI
 
  example usage
- tagAMI region: 'ap-southeast-2',  ami: 'xyz', tags: ['status':'verifed']
+  tagAMI(
+    region: 'ap-southeast-2',             # Required
+    tags: ['Status':'Verified'],          # Required
+    ami: 'xyz',                           # Optional
+    accounts: ['111111111','222222222'],  # Optional
+    role: 'ciinabox'                      # Optional
+  )
  ************************************/
  @Grab(group='com.amazonaws', module='aws-java-sdk-ec2', version='1.11.198')
 
@@ -14,12 +20,22 @@ import com.amazonaws.regions.*
 
 def call(body) {
   def config = body
+  def config.role = config.get('role', 'ciinabox')
+  def config.accounts = config.get('accounts', [])
+  def client = setupEC2Client(config.region)
+
   node {
     println config
     if(config.ami) {
-      addTags(config.region, config.ami, config.tags)
+      addTags(client, config.region, config.ami, config.tags)
+
+      config.accounts.each { accountId ->
+        client = setupEC2Client(config.region, accountId, config.role)
+        addTags(client, config.region, config.ami, config.tags, accountId)
+      }
     } else {
       //Hack to read ALL env vars
+      println "No AMI was specified. Collecting baked AMIs..."
       envVars = [:]
       sh 'env > env.txt'
       readFile('env.txt').split("\r?\n").each {
@@ -27,26 +43,30 @@ def call(body) {
         envVars[e[0]] = e[1]
       }
       amis = findAMIsInEnvironment(envVars)
+      println "Found ${amis.size()} baked AMIs to tag: ${amis}"
       amis.each { ami ->
-        addTags(config.region, ami, config.tags)
+        addTags(client, config.region, ami, config.tags)
       }
     }
   }
 }
 
 @NonCPS
-def addTags(region, ami, tags) {
-  println "adding tags to ami: ${ami}"
-  def ec2 = AmazonEC2ClientBuilder.standard()
-    .withRegion(region)
-    .build()
+def addTags(client, region, ami, tags, accountId = null) {
+  if (accountId) {
+    println "Adding tags to ami: ${ami} in account ${accountId}"
+  }
+  else {
+    println "Adding tags to ami: ${ami}"
+  }
+
 
   def newTags = []
   tags.each { key, value ->
     newTags << new Tag(key, value)
   }
 
-  ec2.createTags(new CreateTagsRequest()
+  client.createTags(new CreateTagsRequest()
     .withResources(ami)
     .withTags(newTags)
   )
@@ -61,4 +81,51 @@ def findAMIsInEnvironment(environment) {
     }
   }
   return amis
+}
+
+// TODO: put the below functions in a helper class
+@NonCPS
+def setupEC2Client(region, awsAccountId = null, role = null) {
+  def cb = AmazonEC2ClientBuilder.standard().withRegion(region)
+  def creds = getCredentials(awsAccountId, region, role)
+  if(creds != null) {
+    cb.withCredentials(new AWSStaticCredentialsProvider(creds))
+  }
+  return cb.build()
+}
+
+@NonCPS
+def getCredentials(awsAccountId, region, roleName) {
+  def env = System.getenv()
+  if(env['AWS_SESSION_TOKEN'] != null) {
+    return new BasicSessionCredentials(
+      env['AWS_ACCESS_KEY_ID'],
+      env['AWS_SECRET_ACCESS_KEY'],
+      env['AWS_SESSION_TOKEN']
+    )
+  } else if(awsAccountId != null && roleName != null) {
+    def stsCreds = assumeRole(awsAccountId, region, roleName)
+    return new BasicSessionCredentials(
+      stsCreds.getAccessKeyId(),
+      stsCreds.getSecretAccessKey(),
+      stsCreds.getSessionToken()
+    )
+  } else {
+    return null
+  }
+}
+
+@NonCPS
+def assumeRole(awsAccountId, region, roleName) {
+  def roleArn = "arn:aws:iam::" + awsAccountId + ":role/" + roleName
+  def roleSessionName = "sts-session-" + awsAccountId
+  println "assuming IAM role ${roleArn}"
+  def sts = new AWSSecurityTokenServiceClient()
+  if (!region.equals("us-east-1")) {
+      sts.setEndpoint("sts." + region + ".amazonaws.com")
+  }
+  def assumeRoleResult = sts.assumeRole(new AssumeRoleRequest()
+            .withRoleArn(roleArn).withDurationSeconds(3600)
+            .withRoleSessionName(roleSessionName))
+  return assumeRoleResult.getCredentials()
 }
